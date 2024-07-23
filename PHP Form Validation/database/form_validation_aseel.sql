942ba5f23a9cc2016dc9734caa8ff6427633a243
Aseel Aburumman <aseelaburumman49@gmail.com>
2024-07-23 15:06:36 +0300

    Add files via upload

--- PHP Form Validation/database/form_validation_aseel.sql
+++ PHP Form Validation/database/form_validation_aseel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 23, 2024 at 01:02 AM
+-- Generation Time: Jul 23, 2024 at 02:06 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -53,17 +53,21 @@ CREATE TABLE `users` (
   `mobile` int(20) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `role_id` int(200) DEFAULT NULL,
-  `profile_picture` varchar(255) DEFAULT NULL
+  `profile_picture` longblob DEFAULT NULL,
+  `Date_created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`users_id`, `Full_name`, `email`, `mobile`, `password`, `role_id`, `profile_picture`) VALUES
-(1, 'Fajer Basheer Falah Hasan', 'aseeaburumman47@gmail.com', 796615575, '$2y$10$X9kH5yxjaetIZyWi0Zgu3O7Wd2nWVE2jwMSkbsF1KA8Ozv.WFxsca', 1, NULL),
-(2, 'Aseel Ibrahim Ali Aburumman', 'aseelaburumman49@gmail.com', 796615575, '$2y$10$X9kH5yxjaetIZyWi0Zgu3O7Wd2nWVE2jwMSkbsF1KA8Ozv.WFxsca', 2, NULL),
-(7, 'Samer Seran Rafi Aljbour', 'samer@gmail.com', 796615575, '$2y$10$T.qMO4MS7STtDGydo7TES.gtFwN2bMDcxzVrZj9akdXzqNCMwoGeq', 1, 'sadcat.png');
+INSERT INTO `users` (`users_id`, `Full_name`, `email`, `mobile`, `password`, `role_id`, `profile_picture`, `Date_created`) VALUES
+(1, 'Fajer Basheer Falah Hasan', 'aseeaburumman47@gmail.com', 796615575, '$2y$10$X9kH5yxjaetIZyWi0Zgu3O7Wd2nWVE2jwMSkbsF1KA8Ozv.WFxsca', 1, NULL, '2024-07-23 10:23:03'),
+(2, 'Aseel Ibrahim Ali Aburumman', 'aseelaburumman49@gmail.com', 796615575, '$2y$10$X9kH5yxjaetIZyWi0Zgu3O7Wd2nWVE2jwMSkbsF1KA8Ozv.WFxsca', 2, NULL, '2024-07-23 10:23:03'),
+(8, 'lana ahmad ali moham', 'lana@gmail.com', 796615575, '$2y$10$cYyuI5toAs0Jv16ef1cjwu9YssSsh6gw.ky2.LcgooV5UBwSuvfVS', 1, '', '2024-07-23 10:23:03'),
+(12, 'Belal Khaled Khaer Naqawa', 'belal@gmail.com', 796615575, '$2y$10$o3G3sOlxMbDlMLq11lPrfeZgFLR2x1jzc1tNuMdRQ.k1MM5JQc8Xu', 1, 0x75706c6f616465645f70696374757265732f736164636174322e706e67, '2024-07-23 10:23:03'),
+(13, 'Amal Bashar Mohammad Alhamdan', 'amal@gmail.com', 796615575, '$2y$10$jUfWgWV395gVdmr3suybW.GtJvg4mXobKn9zPhgN3DkEIkY.c69DW', 1, 0x75706c6f616465645f70696374757265732f736164636174322e706e67, '2024-07-23 10:26:46'),
+(14, 'Osama Ahmad Mahmoud Alhyari', 'osama@gmail.com', 796615575, '$2y$10$yQsFBUnRz77tVXH9zT2pOeUnbOBg6DxK6.WH1mimuQn33ZZXEAXh.', 1, NULL, '2024-07-23 10:44:42');
 
 --
 -- Indexes for dumped tables
@@ -97,7 +101,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
